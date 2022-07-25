16440c93ea474f639fb79e418d5c86bf59fecf87
Oriol Marí <oriolmarimarques@gmail.com>
2022-07-25 12:53:51 +0200

    Use v1beta1

--- eks-nearbyone/main.tf
+++ eks-nearbyone/main.tf
@@ -7,7 +7,7 @@ provider "helm" {
     host                   = module.eks_cluster.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
     exec {
-      api_version = "client.authentication.k8s.io/v1alpha1"
+      api_version = "client.authentication.k8s.io/v1beta1"
       args        = ["eks", "get-token", "--cluster-name", local.config["cluster_name"], "--region", local.config["aws_region"]]
       command     = "aws"
     }
@@ -18,7 +18,7 @@ provider "kubernetes" {
   host                   = module.eks_cluster.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_cluster.cluster_certificate_authority_data)
   exec {
-    api_version = "client.authentication.k8s.io/v1alpha1"
+    api_version = "client.authentication.k8s.io/v1beta1"
     args        = ["eks", "get-token", "--cluster-name", local.config["cluster_name"], "--region", local.config["aws_region"]]
     command     = "aws"
   }
